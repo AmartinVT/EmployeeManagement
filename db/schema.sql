29ad102d47ac0c24ee1f83ff93efc6296f0f57d5
VT_AOE_91 <amartin80108@gmail.com>
2022-07-31 14:40:42 -0400

    Created DB schema per homework-demo-01.png

--- db/schema.sql
+++ db/schema.sql
@@ -7,10 +7,31 @@ CREATE DATABASE employee_db;
 -- Use the employee_db --
 USE employee_db;
 
--- Creates the table "emp_names" within books_db --
-CREATE TABLE emp_names (
+-- Creates the table "employee" within employee_db --
+CREATE TABLE employee (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT,
+  -- Makes columns for first name, last name, role ID, and manager ID --
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  role_id INT,
+  manager_id INT
+);
+
+-- Creates the table "department" within employee_db --
+CREATE TABLE department (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
   id INT,
   -- Makes a string column called "name" which cannot contain null --
   name VARCHAR(100)
+);
+
+-- Creates the table "role" within employee_db --
+CREATE TABLE role (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT,
+  -- Columns for title, salary, and department ID --
+  title VARCHAR(100),
+  salary DECIMAL,
+  department_id INT
 );
---
+++ index.js
@@ -0,0 +1,12 @@
+// Dependencies
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const tables = require('console.table');
+
+// Server connection
+
+// Server interactions
+
+// GET
+
+// POST
