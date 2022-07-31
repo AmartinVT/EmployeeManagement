3d7af854ecf6e4f9a390033b9d9c138e527c2073
VT_AOE_91 <amartin80108@gmail.com>
2022-07-31 16:41:06 -0400

    Added user input prompt function and top level menu options.

--- index.js
+++ index.js
@@ -14,7 +14,27 @@ const db = createConnection(
     },
     console.log("Connection successful!!!")
     );
+
+// User prompts
+function userInput(){
+    inquirer.prompt({
+        name: 'userOptions',
+        type: 'list',
+        message: 'Welcome! Please select an option from the below list using ARROW KEYS + ENTER',
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role"
+        ],
+    }).then()
+}
+
 // Server interactions
+
 
 // GET
 
