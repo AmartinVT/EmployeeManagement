be5d2eb76280ab2edc92f2f33adc4c50fdbd37da
VT_AOE_91 <amartin80108@gmail.com>
2022-07-31 15:05:56 -0400

    Fixed dependency formatting, added server connection syntax to index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Dependencies
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const tables = require('console.table');
+import inquirer from 'inquirer';
+import { createConnection } from 'mysql2';
+import tables from 'console.table';
 
 // Server connection
-
+const db = createConnection(
+    {
+    host: 'localhost',
+    user: 'root',
+    password: 'porkchop',
+    database: 'MartinMacAir',
+    multipleStatements: true
+    },
+    console.log("Connection successful!!!")
+    );
 // Server interactions
 
 // GET
